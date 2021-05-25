e7c325d80db3c17ccbb4cdb5485a9e699088e1d5
Lesley Huang <ningning621@gmail.com>
2021-05-25 01:39:03 -0400

    cycle annotation

--- cycleVis.js
+++ cycleVis.js
@@ -19,6 +19,17 @@ function drawSleepCycleChart(svgClass) {
     })
     .innerRadius(innerRad)
     .outerRadius(circleSize*0.55);
+  let hover_arc = d3.arc()
+    .startAngle(function(d) {
+      var str = d["Hours"].split("-");
+      return (convertTimeStrToFrac(str[0])/24 * 360 * PI / 180);
+    })
+    .endAngle(function(d) {
+      var str = d["Hours"].split("-");
+      return (convertTimeStrToFrac(str[1])/24 * 360 * PI / 180);
+    })
+    .innerRadius(innerRad)
+    .outerRadius(circleSize*0.58);
 
   let data = {
     "monophasic": [
@@ -52,6 +63,13 @@ function drawSleepCycleChart(svgClass) {
 
   let cycles = ["monophasic", "biphasic", "everyman", "dymaxion", "uberman"];
   let hours = ["8 hours", "7.5 hours", "4.5 hours", "2 hours", "2 hours"];
+  let breakdown = {
+    "monophasic": ["8h"], 
+    "biphasic": ["6h", "1.5h"], 
+    "everyman": ["3.5h", "20min", "20min", "20min"], 
+    "dymaxion": ["30min", "30min", "30min", "30min"], 
+    "uberman" : ["20min", "20min", "20min", "20min", "20min", "20min"]
+  }
   let count = 0;
 
   // draw x-axis
@@ -62,23 +80,117 @@ function drawSleepCycleChart(svgClass) {
   for (var i = (circleSpacing + circleSize*0.5); i < svgWidth; i = i+(circleSize+circleSpacing*2)) {
     // add background circle 
     svg.append("path")
+      .attr("cycle", cycles[count])
       .attr("d", d3.arc()
         .innerRadius(innerRad) // hardcoded
         .outerRadius(circleSize*0.5) // hardcoded
         .startAngle(0)
         .endAngle(360 * PI / 180))
       .attr('transform', "translate(" + i + ", " + (svgHeight*0.6) + ")")
-      .style("fill", backgroundColor);
+      .style("fill", backgroundColor)
+      .on("mouseover", function() {
+        let id = d3.select(this).attr("cycle");
+
+        // handle circle animation
+        d3.selectAll("." + id)
+          .transition()
+          .duration(500)
+          .attr("d", hover_arc);
+
+        // handle text animation
+        d3.selectAll(".text_" + id)
+          .transition()
+          .duration(300)
+          .style("opacity", 1);
+      })
+      .on("mouseout", function() {
+        let id = d3.select(this).attr("cycle");
+
+        // handle circle animation
+        d3.selectAll("." + id)
+          .transition()
+          .duration(500)
+          .attr("d", arc);
+
+        // handle text animation
+        d3.selectAll(".text_" + id)
+          .transition()
+          .duration(300)
+          .style("opacity", 0);
+      });
     
     // draw arcs for sleep time
     svg.selectAll(".marker")
       .data(data[cycles[count]])
       .enter()
       .append("path")
+      .attr("class", cycles[count])
       .attr("d", arc)
       .attr('transform', "translate(" + i + ", " + (svgHeight*0.6) + ")")
       .style("fill", blueArcColor)
-      .style("opacity", 0.8);
+      .style("opacity", 0.8)
+      .on("mouseover", function() {
+        let id = d3.select(this).attr("class");
+
+        // handle circle animation
+        d3.selectAll("." + id)
+          .transition()
+          .duration(500)
+          .attr("d", hover_arc);
+        
+        // handle text animation
+        d3.selectAll(".text_" + id)
+          .transition()
+          .duration(300)
+          .style("opacity", 1);
+      })
+      .on("mouseout", function() {
+        let id = d3.select(this).attr("class");
+
+        // handle circle animation
+        d3.selectAll("." + id)
+          .transition()
+          .duration(500)
+          .attr("d", arc);
+
+        // handle text animation
+        d3.selectAll(".text_" + id)
+          .transition()
+          .duration(300)
+          .style("opacity", 0);
+      });
+
+    svg.selectAll(".markerText")
+      .data(data[cycles[count]])
+      .enter()
+      .append("text")
+      .attr("class", "text_" + cycles[count])
+      .text(function(d, i) {
+        let cycle = cycles[count];
+        return breakdown[cycle][i];
+      })
+      .attr("transform", function(d) {
+        var pos = arc.centroid(d);
+        var x = pos[0];
+        var y = pos[1];
+        var hyp = Math.sqrt(x*x + y*y);
+        return 'translate(' + (i + x/hyp*(circleSize*0.55+15)) + "," + ((svgHeight*0.6) + y/hyp*(circleSize*0.55+15))+ ')';
+      })
+      .style('text-anchor', function(d) {
+        var str = d["Hours"].split("-");
+        d["startAngle"] = convertTimeStrToFrac(str[0])/24 * 360 * PI / 180;
+        d["endAngle"] = convertTimeStrToFrac(str[1])/24 * 360 * PI / 180;
+
+        return (midAngle(d)) < Math.PI ? 'start' : 'end';
+      })
+      .style("font-family", "Rubik")
+      .style("font-weight", "bold")
+      .style("font-size", 12)
+      .style("opacity", 0)
+      .style("fill", textColor);
+
+    //var str = d["Hours"].split("-");
+    //return (convertTimeStrToFrac(str[0])/24 * 360 * PI / 180);
 
     // add title for sleep cycle
     svg.append("text")
@@ -136,3 +248,5 @@ function drawSleepCycleChart(svgClass) {
     "duration and provides flexibility in how someone can manage their sleep hours (3). "
   ]);
 }
+
+function midAngle(d) { return d.startAngle + (d.endAngle - d.startAngle) / 2; }
